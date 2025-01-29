42140ea2eb27db976661ffbf8dabf07d08257bfa
nabilWafick <nabilsoumanou7@gmail.com>
2025-01-29 12:05:36 +0100

    remove uned useParams

--- src/app/[locale]/projects/[id]/page.tsx
+++ src/app/[locale]/projects/[id]/page.tsx
@@ -4,7 +4,6 @@ import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { File } from "lucide-react";
 import Link from "next/link";
-import { useParams } from "next/navigation";
 import { FC } from "react";
 import { FaEnvelope, FaFacebook, FaLink } from "react-icons/fa";
 
